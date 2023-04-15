2c657c0d479b0cf3cf526752bc1d3c25cce6e415
Giulia Angeli <giuliaangelioliveira@gmail.com>
2023-04-15 19:21:01 -0300

    add jquery-mask-plugin

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,19 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+window.$ = window.jQuery = require('jquery');
+require("jquery-mask-plugin");
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.maskFields();
+  }
+  
+  maskFields() {
+    $('[data-masks-target="phone"]').mask('(00) 00000-0009');
+    $('[data-masks-target="cpf"]').mask('000.000.000-00');;
+  }
+}
